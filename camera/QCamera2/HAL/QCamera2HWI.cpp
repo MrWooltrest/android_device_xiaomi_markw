667e8988909d9ec09a0b586839eea3094c69adf7
Guruprasad Gaonkar <ggaonk@codeaurora.org>
2016-06-20 17:17:55 -0700

    markw: QCamera2: Add buffer identity for video native handle.
    Issue: Release_recording() can be called with any Imemory pointer
    sent earlier. HAL cannot use this pointer to recognize video frame
    to match before release this buffer to kernel.
    
    Fix: Buffer identity is introduced to every video buffer and released
    buffer can be found by comparing identity.
    
    CRs-Fixed: 1026840
    Change-Id: I287d1276fbf2241a2fc6d470e887a91e1daeca4d

--- camera/QCamera2/HAL/QCamera2HWI.cpp
+++ camera/QCamera2/HAL/QCamera2HWI.cpp
@@ -696,8 +696,6 @@ void QCamera2HardwareInterface::stop_recording(struct camera_device *device)
     LOGI("[KPI Perf]: E PROFILE_STOP_RECORDING camera id %d",
              hw->getCameraId());
 
-    mVideoMem = NULL;
-
     hw->lockAPI();
     qcamera_api_result_t apiResult;
     int32_t ret = hw->processAPI(QCAMERA_SM_EVT_STOP_RECORDING, NULL);
@@ -770,17 +768,6 @@ void QCamera2HardwareInterface::release_recording_frame(
         return;
     }
     LOGD("E camera id %d", hw->getCameraId());
-
-    //Close and delete duplicated native handle and FD's.
-    if ((hw->mVideoMem != NULL) && (hw->mStoreMetaDataInFrame)) {
-         ret = hw->mVideoMem->closeNativeHandle(opaque, TRUE);
-        if (ret != NO_ERROR) {
-            LOGE("Invalid video metadata");
-            return;
-        }
-    } else {
-        LOGW("Possible FD leak. Release recording called after stop");
-    }
 
     hw->lockAPI();
     qcamera_api_result_t apiResult;
@@ -1676,7 +1663,6 @@ QCamera2HardwareInterface::QCamera2HardwareInterface(uint32_t cameraId)
       mJpegClientHandle(0),
       mJpegHandleOwner(false),
       mMetadataMem(NULL),
-      mVideoMem(NULL),
       mNextJobId(1),
       mCACDoneReceived(false),
       m_bNeedRestart(false),
@@ -2854,10 +2840,6 @@ QCameraMemory *QCamera2HardwareInterface::allocateStreamBuf(
             }
             videoMemory->setVideoInfo(usage, fmt);
             mem = videoMemory;
-            if (!mParameters.getBufBatchCount()) {
-                //For batch mode this will be part of user buffer.
-                mVideoMem = videoMemory;
-            }
         }
         break;
     case CAM_STREAM_TYPE_CALLBACK:
@@ -3188,7 +3170,6 @@ QCameraMemory *QCamera2HardwareInterface::allocateStreamUserBuf(
         }
         video_mem->setVideoInfo(usage, fmt);
         mem = static_cast<QCameraMemory *>(video_mem);
-        mVideoMem = video_mem;
     }
     break;
 
@@ -3627,7 +3608,6 @@ int QCamera2HardwareInterface::startRecording()
     int32_t rc = NO_ERROR;
 
     LOGI("E");
-    mVideoMem = NULL;
     //link meta stream with video channel if low power mode.
     if (isLowPowerMode()) {
         // Find and try to link a metadata stream from preview channel
