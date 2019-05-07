616e5af4ede1d1659d9cca377afabff170699faf
Tao Bao <tbao@google.com>
2019-05-06 22:42:02 -0700

    markw: AB_OTA_UPDATER is BoardConfig variable.
    Bug: 130433003
    Test: TreeHugger
    Change-Id: I70c7a884df45781e8b14339edd02985ba3de02fd

--- lineage_markw.mk
+++ lineage_markw.mk
@@ -20,7 +20,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Inherit from markw device
-AB_OTA_UPDATER := false
 $(call inherit-product, device/xiaomi/markw/device.mk)
 
 # Inherit some common LineageOS stuff.
