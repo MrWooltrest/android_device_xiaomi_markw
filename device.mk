2042039a066884b8a43aaeebded65ac17fcd3dba
Daniel Rosenberg <drosen@google.com>
2020-01-29 19:40:07 -0800

    markw: Enable Project ID Quota support on userdata
    These features enable wide inodes on userdata, which is required for
    project id quotas. Wide inodes can only be set at mkfs time.
    
    Bug: 138321217
    Test: Examine filesystem features for project quota and wide inodes
    Change-Id: I9c17cda2af34c6fbd73c2d90eb6b465f4939df67

--- device.mk
+++ device.mk
@@ -166,6 +166,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gpfspath_oem_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gpfspath_oem_config.xml
 
+# Enable wider inodes for project quotas
+# Later, replace with $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+PRODUCT_QUOTA_PROJID := 1
+PRODUCT_PRODUCT_PROPERTIES += ro.emulated_storage.projid=1
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi_markw
