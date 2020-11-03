fe5becc63a71245e4a119394ba8cca6502bd2bb5
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2020-11-03 16:08:57 +0800

    markw: camera: Add header library libandroid_sensor_headers
    Change-Id: I6d45e2994127c21c14a98d9b0d5f8a7031ef2f65

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -119,6 +119,8 @@ endif
 
 LOCAL_STATIC_LIBRARIES := android.hardware.camera.common@1.0-helper
 
+LOCAL_HEADER_LIBRARIES += libandroid_sensor_headers
+
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_VENDOR_MODULE := true
