1d5c332636c4f53c8d6cc4ae5c8cefdc3fdec5f9
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-08 20:01:56 +0800

    markw: keylayout: Kang keylayouts from ugg
    [wltrest]: Omit gf3208.kl. We already have it
    
    Change-Id: I0ee77f7ac960f3f96393564baf167028e23bc210

--- device.mk
+++ device.mk
@@ -219,6 +219,7 @@ $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_markw)
 
 # Input
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayout/fts_ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/fts_ts.kl \
     $(LOCAL_PATH)/configs/keylayout/gf3208.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gf3208.kl \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/configs/keylayout/msm8953-snd-card-mtp_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/msm8953-snd-card-mtp_Button_Jack.kl \
