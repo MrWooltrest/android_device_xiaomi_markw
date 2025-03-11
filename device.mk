6546d0a8a5019aa1b2d287b8464c5fa114909cfb
Nolen Johnson <johnsonnolen@gmail.com>
2025-03-11 12:54:01 -0400

    markw: Drop old Android Go configurations
    * These break build on QPR2.
    
    Change-Id: Ib2a80892f18d117e27dd703fa74122b24d900dcf

--- device.mk
+++ device.mk
@@ -230,10 +230,6 @@ PRODUCT_PACKAGES += \
     ims-ext-common \
     ims_ext_common.xml
 
-# Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/ft5435_ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ft5435_ts.kl \
