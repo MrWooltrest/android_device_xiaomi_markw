2ae8a9956303886895c8ed343c6851fb2b2c327b
LuK1337 <priv.luk@gmail.com>
2025-08-21 19:01:20 +0200

    markw: Migrate to AIDL camera provider
    Change-Id: I290ecfd9760d6428fe64f137974c353846c0e35d

--- device.mk
+++ device.mk
@@ -128,8 +128,7 @@ TARGET_BOOTANIMATION_HALF_RES := true
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider-service_32.lineage \
     camera.msm8953 \
     libwui
 
