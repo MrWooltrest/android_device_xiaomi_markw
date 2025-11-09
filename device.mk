74459a5b044bd07b296d6b70bdf18787ae41a366
wltrest <wooltrest@gmail.com>
2025-11-09 21:43:23 +0200

    markw: Don't build Camera HAL for now
    Change-Id: Ifd959500718d674839117b0e386f9e1303478bd8

--- device.mk
+++ device.mk
@@ -141,8 +141,8 @@ TARGET_BOOTANIMATION_HALF_RES := true
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    camera.msm8953
+    android.hardware.camera.provider@2.4-service
+#   camera.msm8953
 
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
