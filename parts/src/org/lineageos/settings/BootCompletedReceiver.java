19a8391e322265bd4ccb5311683798bca114fa2a
wltrest <wooltrest@gmail.com>
2025-12-10 18:30:34 +0200

    markw: parts: Nuke sound control
    Change-Id: I5bd5289efa26752a42dabf1ac748304f0fee7c9a

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -28,7 +28,6 @@ import androidx.preference.PreferenceManager;
 
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.preferences.FileUtils;
-import org.lineageos.settings.soundcontrol.SoundControlSettings;
 import org.lineageos.settings.torch.TorchSettings;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -62,13 +61,5 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         FileUtils.setValue(TorchSettings.TORCH_2_BRIGHTNESS_PATH,
                 Settings.Secure.getInt(context.getContentResolver(),
                         TorchSettings.KEY_YELLOW_TORCH_BRIGHTNESS, 100));
-
-        int gain = Settings.Secure.getInt(context.getContentResolver(),
-                SoundControlSettings.PREF_HEADPHONE_GAIN, 4);
-        FileUtils.setValue(SoundControlSettings.HEADPHONE_GAIN_PATH, gain + " " + gain);
-        FileUtils.setValue(SoundControlSettings.MICROPHONE_GAIN_PATH, Settings.Secure.getInt(context.getContentResolver(),
-                SoundControlSettings.PREF_MICROPHONE_GAIN, 0));
-        FileUtils.setValue(SoundControlSettings.SPEAKER_GAIN_PATH, Settings.Secure.getInt(context.getContentResolver(),
-                SoundControlSettings.PREF_SPEAKER_GAIN, 0));
     }
 }
--- parts/src/org/lineageos/settings/PartsSearchIndexablesProvider.java
+++ parts/src/org/lineageos/settings/PartsSearchIndexablesProvider.java
@@ -33,7 +33,6 @@ import static android.provider.SearchIndexablesContract.INDEXABLES_RAW_COLUMNS;
 import static android.provider.SearchIndexablesContract.INDEXABLES_XML_RES_COLUMNS;
 import static android.provider.SearchIndexablesContract.NON_INDEXABLES_KEYS_COLUMNS;
 
-import org.lineageos.settings.soundcontrol.SoundControlSettingsActivity;
 import org.lineageos.settings.torch.TorchSettingsActivity;
 
 import java.util.HashSet;
@@ -46,8 +45,6 @@ public class PartsSearchIndexablesProvider extends SearchIndexablesProvider {
 
     static {
         final int rank = 1, iconResId = 0;
-        sResMap.add(new SearchIndexableResource(rank, R.xml.soundcontrol_settings,
-                    SoundControlSettingsActivity.class.getName(), iconResId));
         sResMap.add(new SearchIndexableResource(rank, R.xml.torch_settings,
                     TorchSettingsActivity.class.getName(), iconResId));
     }
